ca68eea47612501e47e3a331a81ed4c3899002ab
Rishikesh <rishikeshjagadale19@gmail.com>
2024-10-13 18:58:07 +0530

    Chapter 2

--- course/2-variables/exercises/1-basic_types/code.go
+++ course/2-variables/exercises/1-basic_types/code.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// initialize variables here
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
 
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
--- course/2-variables/exercises/2-short_declarations/code.go
+++ course/2-variables/exercises/2-short_declarations/code.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	// declare here
-
+	congrats := "Happy birthday!"
 	fmt.Println(congrats)
 }
